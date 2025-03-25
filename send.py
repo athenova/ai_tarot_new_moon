bb6dc47f15e9194a810f53dff30f43e57a189d5e
athenova <aleksey.sergeyev@yandex.com>
2025-03-25 20:27:25 +0300

    Поправлено расписание

--- send.py
+++ send.py
@@ -1,4 +1,4 @@
-from project import Project
+from project import *
 import schedule
 import time
 import ephem
@@ -19,18 +19,18 @@ if __name__ == '__main__':
             if group_id is not None:
                 vk.send(type=type, group_id=group_id, image_gen=False, text_gen=False)
 
-        schedule.every().day.at("21:00",'Europe/Moscow').do(job, type="pisces", chat_id='@pisces_the', group_id='229837683')
-        schedule.every().day.at("21:01",'Europe/Moscow').do(job, type="aries", chat_id='@aries_the', group_id='229837854')
-        schedule.every().day.at("21:02",'Europe/Moscow').do(job, type="taurus")
-        schedule.every().day.at("21:03",'Europe/Moscow').do(job, type="gemini", chat_id='@gemini_the', group_id='229837895')
-        schedule.every().day.at("21:04",'Europe/Moscow').do(job, type="cancer")
-        schedule.every().day.at("21:05",'Europe/Moscow').do(job, type="leo")
-        schedule.every().day.at("21:06",'Europe/Moscow').do(job, type="virgo")
-        schedule.every().day.at("21:07",'Europe/Moscow').do(job, type="libra")
-        schedule.every().day.at("21:08",'Europe/Moscow').do(job, type="scorpio")
-        schedule.every().day.at("21:09",'Europe/Moscow').do(job, type="sagittarius")
-        schedule.every().day.at("21:10",'Europe/Moscow').do(job, type="capricorn", chat_id='@capricorn_the', group_id='229837876')
-        schedule.every().day.at("21:11",'Europe/Moscow').do(job, type="aquarius", chat_id='@aquarius_the', group_id='229837930')
+        schedule.every().day.at("20:30",'Europe/Moscow').do(job, type="pisces", chat_id='@pisces_the', group_id='229837683')
+        schedule.every().day.at("20:31",'Europe/Moscow').do(job, type="aries", chat_id='@aries_the', group_id='229837854')
+        schedule.every().day.at("20:32",'Europe/Moscow').do(job, type="taurus")
+        schedule.every().day.at("20:33",'Europe/Moscow').do(job, type="gemini", chat_id='@gemini_the', group_id='229837895')
+        schedule.every().day.at("20:34",'Europe/Moscow').do(job, type="cancer")
+        schedule.every().day.at("20:35",'Europe/Moscow').do(job, type="leo")
+        schedule.every().day.at("20:36",'Europe/Moscow').do(job, type="virgo")
+        schedule.every().day.at("20:37",'Europe/Moscow').do(job, type="libra")
+        schedule.every().day.at("20:38",'Europe/Moscow').do(job, type="scorpio")
+        schedule.every().day.at("20:39",'Europe/Moscow').do(job, type="sagittarius")
+        schedule.every().day.at("20:40",'Europe/Moscow').do(job, type="capricorn", chat_id='@capricorn_the', group_id='229837876')
+        schedule.every().day.at("20:41",'Europe/Moscow').do(job, type="aquarius", chat_id='@aquarius_the', group_id='229837930')
 
         fifteen_minutes = 15 * 60
 
